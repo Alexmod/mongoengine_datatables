8970ff3cf3c0ba1a5964f244972f5a96e6979781
Alexmod <aimodding@gmail.com>
2021-02-22 09:04:30 +0300

    Mon Feb 22 09:04:30 MSK 2021

--- mongoengine_datatables/__init__.py
+++ mongoengine_datatables/__init__.py
@@ -1,3 +1,3 @@
 '''import'''
-__version__ = '0.0.1'
+__version__ = '0.0.3'
 from .serverside_datatables import DataTables
--- mongoengine_datatables/serverside_datatables.py
+++ mongoengine_datatables/serverside_datatables.py
@@ -95,36 +95,71 @@ class DataTables(object):
         return self.requested_columns[_order_col]
 
     @property
+    def dt_column_search(self):
+        '''
+        Adds support for datatables own column search functionality.
+        documented here: https://datatables.net/examples/api/multi_filter.html
+        '''
+        return [{"column": column['data'],
+                 "value":  column['search']['value'],
+                 "regex":  column['search']['regex']
+                 } for column in self.request_args.get('columns')
+                if column['search']['value'] != ""]
+
+    def query_by_col_type(self, _q, col):
+        '''Build a query depending on the field type'''
+        escaped = re.escape(_q)
+        regexp = re.compile(f'.*{escaped}.*', re.IGNORECASE)
+
+        if self.field_type_dict.get(col) == 'number':
+            if _q.isdigit():
+                return [Q(**{col + '__icontains': _q})]
+            return []
+        if self.field_type_dict.get(col) == 'embed':
+            if not self.embed_search:
+                return []
+            _em = []
+            for field in self.embed_search.get(col)['fields']:
+                _emb = f'{col}__{field}__icontains'
+                _em.append(Q(**{_emb: _q}))
+            return _em
+
+        return [Q(**{col: regexp})]
+
+    @property
     def get_search_query(self):
+        '''Create search request'''
+        queries = []
         _column_names = [d['data']
                          for d in self.columns if d['data']
                          in self.field_type_dict.keys()]
-        queries = []
-        for col in _column_names:
-            _q = self.search_string
-            if self.field_type_dict.get(col) == 'number':
-                if _q.isdigit():
-                    queries.append(Q(**{col + '__icontains': _q}))
-            elif self.field_type_dict.get(col) == 'objectID':
-                continue
-            elif self.field_type_dict.get(col) == 'embed':
-                if not self.embed_search:
-                    continue
-                for field in self.embed_search.get(col)['fields']:
-                    _emb = f'{col}__{field}__icontains'
-                    queries.append(Q(**{_emb: _q}))
-            elif self.field_type_dict.get(col) == 'list':
-                escaped = re.escape(_q)
-                regexp = re.compile(f'.*{escaped}.*', re.IGNORECASE)
-                queries.append(Q(**{col: regexp}))
-            else:
-                queries.append(Q(**{col + '__icontains': _q}))
+        # global search for all columns
+        if self.search_string:
+            for col in _column_names:
+                _q = self.search_string
+                _obj = self.query_by_col_type(_q, col)
+                queries += _obj
+
+        # search by specific columns
+        _own_col_q = []
+        for column_search in self.dt_column_search:
+            col = column_search['column']
+            term = column_search['value']
+            _obj = self.query_by_col_type(term, col)
+            _own_col_q += _obj
 
         if self.q_obj:
             queries.append(self.q_obj)
+
         _search_query = QCombination(QCombination.OR, queries)
+
+        if _own_col_q:
+            _own_col_q = QCombination(QCombination.OR, _own_col_q)
+            _search_query = (_search_query & _own_col_q)
+
         if self.custom_filter:
             _search_query = (_search_query & Q(**self.custom_filter))
+
         return _search_query
 
     def results(self):
@@ -138,9 +173,7 @@ class DataTables(object):
         return dict(data=data, count=_res.count())
 
     def get_rows(self):
-        return {
-            'recordsTotal': self.total_records,
-            'recordsFiltered': self.results().get('count'),
-            'draw': int(str(self.draw)),
-            'data': self.results().get('data')
-        }
+        return {'recordsTotal': self.total_records,
+                'recordsFiltered': self.results().get('count'),
+                'draw': int(str(self.draw)),
+                'data': self.results().get('data')}
